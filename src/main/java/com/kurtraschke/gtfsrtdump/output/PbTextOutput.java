6d4a5f260076c666f0076ca31c6adc4488c38c01
Kurt Raschke <kurt@kurtraschke.com>
2020-05-14 21:54:03 -0400

    Add option to "enhance" pbtext option with a human-readable timestamp.

--- src/main/java/com/kurtraschke/gtfsrtdump/output/PbTextOutput.java
+++ src/main/java/com/kurtraschke/gtfsrtdump/output/PbTextOutput.java
@@ -2,17 +2,47 @@ package com.kurtraschke.gtfsrtdump.output;
 
 import com.google.protobuf.TextFormat;
 import com.google.transit.realtime.GtfsRealtime;
-import picocli.CommandLine;
+import com.kurtraschke.gtfsrtdump.TimestampFormatting;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-@CommandLine.Command(name = "pbtext", description = "Protocol Buffer text format output")
+@Command(name = "pbtext", description = "Protocol Buffer text format output")
 public class PbTextOutput extends OutputMethod {
+    private static final Pattern TIMESTAMP_PATTERN = Pattern.compile("(?:start|end|time(?:stamp)?): (\\d{10})");
+
+    @Option(names = {"--timestamp-format"}, arity = "0..1",
+            fallbackValue = "ISO_8601_LOCAL",
+            description = "Valid values: ${COMPLETION-CANDIDATES}")
+    TimestampFormatting timestampFormatter;
 
     @Override
     public Integer format(GtfsRealtime.FeedMessage fm) throws IOException {
-        TextFormat.print(fm, System.out);
+        String formattedMessage = TextFormat.printToString(fm);
+
+        if (timestampFormatter != null) {
+            formattedMessage = enhanceMessageOutput(formattedMessage);
+        }
+
+        System.out.print(formattedMessage);
 
         return 0;
+    }
+
+    private String enhanceMessageOutput(String formattedMessage) {
+        final Matcher matcher = TIMESTAMP_PATTERN.matcher(formattedMessage);
+
+        final String enhancedMessage = matcher.replaceAll(
+                matchResult -> String.format(
+                        "%s\t/* %s */",
+                        matchResult.group(),
+                        timestampFormatter.formatTimestamp(Integer.parseInt(matchResult.group(1)))
+                )
+        );
+
+        return enhancedMessage;
     }
 }
